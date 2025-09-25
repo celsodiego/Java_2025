e9e96cd2bb471d9e7b10f12ed23eb433cba9a037
DIEGO-LIMA <164493983+celsodiego@users.noreply.github.com>
2025-09-25 16:13:26 +0100

    Add files via upload

---
+++ Actividade_5/src/Actividade_5.java
@@ -0,0 +1,35 @@
+public class Actividade_5 {
+        public static void main (String [] agrs){
+
+                int dia = 5;
+
+                switch (dia){
+                    case 1:
+                        System.out.println("É domingo");
+                        break;
+                    case 2:
+                        System.out.println("É segunda-feira");
+                        break;
+                    case 3:
+                        System.out.println("É terça-feira" );
+                        break;
+                    case 4:
+                        System.out.println("É quarta-feira");
+                        break;
+                    case 5:
+                        System.out.println("É quinta-feira");
+                        break;
+                    case 6:
+                        System.out.println("É sexta-feira");
+                        break;
+                    case 7:
+                        System.out.println("É sábado");
+                        break;
+                    default:
+                        System.out.println("#Não corresponde a um dia de semana");
+                        break;
+                }
+
+        }
+    }
+
