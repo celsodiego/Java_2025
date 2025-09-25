7c4f7809f5f71810ff2af70e4249ebaab9119726
DIEGO-LIMA <164493983+celsodiego@users.noreply.github.com>
2025-09-25 15:51:45 +0100

    Add files via upload

---
+++ Actividade_4/src/Actividade_4.java
@@ -0,0 +1,26 @@
+public class Actividade_4{
+    public static void main (String [] args) {
+
+        char vogal = 'A';
+
+        switch (vogal) {
+            case 'A':
+                System.out.println("É uma vogal");
+                break;
+            case 'E':
+                System.out.println("É uma vogal");
+                break;
+            case 'I':
+                System.out.println("É uma vogal");
+                break;
+            case 'O':
+                System.out.println("É uma vogal");
+                break;
+            case 'U':
+                System.out.println("É uma vogal");
+                break;
+            default:
+                System.out.println("É uma consoante");
+        }
+    }
+}
