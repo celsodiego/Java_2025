6da08b5df31fe965ec844737b89d61c1d9810ae4
DIEGO-LIMA <164493983+celsodiego@users.noreply.github.com>
2026-01-23 15:36:13 +0000

    Add files via upload

---
+++ Ex27.java
@@ -0,0 +1,13 @@
+public class Ex27 {
+    public static void main (String [] args){
+
+        String idade = "30";
+
+        int idadeInt = Integer.parseInt(idade);
+
+        float idadefloat = Float.parseFloat(idade);
+
+        System.out.println("valor int: " + idadeInt);
+        System.out.println("valor float: " +idadefloat);
+    }
+}
